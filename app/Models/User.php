904d186dbdfa32694cc411417d27779664885df7
dave de visser <blubdev.llc@gmail.com>
2026-03-25 15:29:45 +0100

    functonality on fillable
    function on verifying the user has been fixed u can now as an mod or admin verify users!

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable(['name', 'email', 'password', 'role', 'biografie', 'contact_info', 'krediet', 'is_verified', 'is_blocked'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
